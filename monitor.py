71d9772fe948982363849043abd38107e43544fb
ferrejacobs22 <fjacobs2007@gmail.com>
2026-08-14 00:31:35 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,9 +1,11 @@
 import time
+import sys
 
-print("================================")
-print("🟢 PKP MONITOR GESTART")
-print("================================")
+print("================================", flush=True)
+print("🟢 PKP MONITOR GESTART", flush=True)
+print("================================", flush=True)
 
 while True:
     print("🔎 Monitor draait...", flush=True)
+    sys.stdout.flush()
     time.sleep(10)
